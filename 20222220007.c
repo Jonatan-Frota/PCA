aa0ed9c8a8504d92c0e6ac2f9f7ba5bb4abf55f2
Jonatan-Frota <120187516+Jonatan-Frota@users.noreply.github.com>
2022-12-22 09:00:32 -0400

    Add files via upload

---
+++ 20222220007.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float n1 = 0, n2 = 0, n3 = 0, media = 0, maiornot = 0, menornot = 0, maiormed = 0, menormed = 0;
+      int fim;
+    printf("* CALCULO DE NOTAS *\n");
+        printf("Insira a primeira, segunda e terceira nota: ");
+        scanf ("%f %f %f", &n1, &n2, &n3);
+        media = (n1 + n2 + n3)/3;
+        if (n1 > maiornot) {
+            maiornot = (maiornot - maiornot) + n1;}
+        if (n2 > maiornot){
+            maiornot = (maiornot - maiornot) + n2;}
+        if (n3 > maiornot){
+            maiornot = (maiornot - maiornot) + n3;
+        }
+
+        if (n1 < n2 && n1 < n3){
+            menornot = (menornot - menornot) + n1;
+        }
+        if (n2 < n1 && n2 < n3){
+            menornot = (menornot - menornot) + n2;
+        }
+        if (n3 < n1 && n3 < n2){
+            menornot = (menornot - menornot) + n3;
+        }
+        if (media > maiormed){
+            maiormed = (maiormed - maiormed) + media;
+        }
+        if (menormed <= menormed) {
+            menormed = (menormed - menormed) + media;
+        }
+        if (n1 == n2 || n1 == n3){
+            printf("A maior nota foi da primeira prova: %1.2f\n", n1);
+        }
+        if (n2 == n3) {
+            printf("A maior nota foi da segunda prova: %1.2f\n", n2);
+        }
+        if (n1 > n2 && n1 > n3){
+            printf("A maior nota foi da primeira prova: %1.2f\n", maiornot);
+        }
+        if (n2 > n1 && n2 > n3){
+            printf("A maior nota foi da segunda prova: %1.2f\n", maiornot);
+        }
+        if (n3 > n2 && n3 >n1){
+            printf("A maior nota foi da terceira prova: %1.2f\n", maiornot);
+        }
+        if (n1 < n2 && n1 < n3){
+            printf("A menor nota foi da primeira prova: %1.2f\n", menornot);
+        }
+        if (n2 < n1 && n2 < n3){
+            printf("A menor nota foi da segunda prova: %1.2f\n", menornot);
+        }
+        if (n3 < n2 && n3 <n1){
+            printf("A menor nota foi da terceira prova: %1.2f\n", menornot);
+        }
+        printf("Media: %1.2f\n", maiormed);
+    return 0;
+}
