93e20a3e2fd7e7828a9f6a2e79fce4bee5d57cb5
Jonatan-Frota <120187516+Jonatan-Frota@users.noreply.github.com>
2023-01-15 18:07:01 -0400

    Add files via upload

---
+++ Questao_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nota, media = 0, i=0, maior = 0, menor = 0, j;
+    printf("Calculo de medias: \n");
+    do{
+    printf("Insira uma nota: ");
+    scanf("%d", &nota);
+    if(nota<0){
+        break;
+    }
+    i++;
+    if (i == 1){
+        menor = nota;
+    }
+
+    media = media + nota;
+    if (nota > maior){
+        maior = (maior - maior) + nota;
+    }
+    if (nota < menor){
+        menor = (menor - menor) + nota;
+    };
+    } while(nota>= 0);
+    printf("\nMedia geral da turma: %1.1f", (media*1.0)/i);
+    printf("\nMaior nota: %d", maior);
+    printf("\nMenor nota: %d", menor);
+
+    return 0;
+}
